9792d3dea52be5cee53e185866ec295be7340da2
Ashish <ashishranjan30@gmail.com>
2022-12-10 22:16:25 +0530

    Added Recursion Excercise 1

---
+++ src/algo/recursion/RecursionTask3MultipleRecursionCalls.java
@@ -0,0 +1,14 @@
+package algo.recursion;
+
+public class RecursionTask3MultipleRecursionCalls {
+	
+	/**
+	 * Print the fibonaci number
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
