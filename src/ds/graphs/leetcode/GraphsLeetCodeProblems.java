d0c55c402e86b0607394346562f87fc7965c6c7e
IN22914548 <ashish.ranjan@tesco.com>
2023-11-16 16:46:56 +0530

    Added Graph Solutions

--- src/ds/graphs/leetcode/GraphsLeetCodeProblems.java
+++ src/ds/graphs/leetcode/GraphsLeetCodeProblems.java
@@ -1,9 +1,12 @@
 package ds.graphs.leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Queue;
 import java.util.Set;
@@ -91,6 +94,160 @@ public class GraphsLeetCodeProblems {
         System.out.println("Actual output is ");
         graphsLeetCodeProblems.solve(charGrid);
         System.out.println(charGrid);
+
+        grid = new int[][]{{1,2,2,3,5},{3,2,3,4,4},{2,4,5,3,1},{6,7,1,4,5},{5,1,1,2,4}};
+        System.out.println();
+        System.out.println("Input is ");
+        System.out.println("{1,2,2,3,5}");
+        System.out.println("{3,2,3,4,4}");
+        System.out.println("{2,4,5,3,1}");
+        System.out.println("{6,7,1,4,5}");
+        System.out.println("{5,1,1,2,4}");
+        System.out.println("Expected output is {{0,4},{1,3},{1,4},{2,2},{3,0},{3,1},{4,0}}");
+        System.out.println("Actual output is " + graphsLeetCodeProblems.pacificAtlantic(grid));
+
+        int numberOfCourses = 2;
+        int[][] prerequisites = new int[][]{{1, 0}};
+        System.out.println();
+        System.out.println("Input is ");
+        System.out.println("number of courses - 2");
+        System.out.println("Prerequisites are ");
+        System.out.println("{1, 0}");
+        System.out.println("Expected output is true");
+        System.out.println("Actual output is " + graphsLeetCodeProblems.canFinish(numberOfCourses, prerequisites));
+
+        numberOfCourses = 2;
+        prerequisites = new int[][]{{1, 0}};
+        System.out.println();
+        System.out.println("Input is ");
+        System.out.println("number of courses - 2");
+        System.out.println("Prerequisites are ");
+        System.out.println("{1, 0}");
+        System.out.println("Expected output is [0, 1]");
+        System.out.println("Actual output is " + Arrays.toString(graphsLeetCodeProblems.findOrder(numberOfCourses,
+                                                                                                prerequisites)));
+
+        numberOfCourses = 4;
+        prerequisites = new int[][]{{1,0},{2,0},{3,1},{3,2}};
+        System.out.println();
+        System.out.println("Input is ");
+        System.out.println("number of courses - 4");
+        System.out.println("Prerequisites are ");
+        System.out.println("{1, 0}");
+        System.out.println("{2, 0}");
+        System.out.println("{3, 1}");
+        System.out.println("{3, 2}");
+        System.out.println("Expected output is [0, 1, 2, 3]");
+        System.out.println("Actual output is " + Arrays.toString(graphsLeetCodeProblems.findOrder(numberOfCourses,
+                                                                                                prerequisites)));
+
+        numberOfCourses = 1;
+        prerequisites = new int[][]{};
+        System.out.println();
+        System.out.println("Input is ");
+        System.out.println("number of courses - 1");
+        System.out.println("Prerequisites are []");
+        System.out.println("Expected output is [0]");
+        System.out.println("Actual output is " + Arrays.toString(graphsLeetCodeProblems.findOrder(numberOfCourses,
+                                                                                                prerequisites)));
+    }
+
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> adjancenyList = new HashMap<>();
+        List<Integer> answer = new ArrayList<>();
+
+        // map each course to : prereq list
+        for (int i = 0; i < numCourses; i++) {
+            adjancenyList.put(i, new ArrayList<>());
+        }
+        for (int i = 0; i < prerequisites.length; i++) {
+            List<Integer> existingPreq = adjancenyList.get(prerequisites[i][0]);
+            existingPreq.add(prerequisites[i][1]);
+            adjancenyList.put(prerequisites[i][0], existingPreq);
+        }
+
+        Set<Integer> visitedSets = new HashSet<>();
+        Set<Integer> cycle = new HashSet<>();
+
+        for (int index = 0; index < numCourses; index++) {
+            if (!isTraversePossible(index, adjancenyList, visitedSets, cycle, answer)) {
+                return new int[]{};
+            }
+        }
+
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    private boolean isTraversePossible(int course, Map<Integer, List<Integer>> adjancenyList, Set<Integer> visitedSets,
+                                       Set<Integer> cycle, List<Integer> answer) {
+        if (cycle.contains(course)) {
+            return false;
+        }
+        if (visitedSets.contains(course)) {
+            return true;
+        }
+
+        cycle.add(course);
+        for (Integer preReq : adjancenyList.get(course)) {
+            if (!isTraversePossible(preReq, adjancenyList, visitedSets, cycle, answer)) {
+                return false;
+            }
+        }
+
+        cycle.remove(course);
+        answer.add(course);
+        visitedSets.add(course);
+        return true;
+    }
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> adjancenyList = new HashMap<>();
+
+        // map each course to : prereq list
+        for (int i = 0; i < numCourses; i++) {
+            adjancenyList.put(i, new ArrayList<>());
+        }
+        for (int i = 0; i < prerequisites.length; i++) {
+            List<Integer> existingPreq = adjancenyList.get(prerequisites[i][0]);
+            existingPreq.add(prerequisites[i][1]);
+            adjancenyList.put(prerequisites[i][0], existingPreq);
+        }
+
+        // Visited Course is a set containing all courses along the current DFS Path
+        // This is to detect cycle
+        Set<Integer> cycle = new HashSet<>();
+        for (Integer course : adjancenyList.keySet()) {
+            // If 1 is false the result would be false, else true
+            if (!isCoursePossibleToFinish(course, adjancenyList, cycle)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean isCoursePossibleToFinish(Integer course, Map<Integer, List<Integer>> adjancenyList, Set<Integer> cycle) {
+        // Base Case for false - Cycle Detection
+        if (cycle.contains(course)) {
+            return false;
+        }
+        // Base Case for true - No Prerequisites
+        if (adjancenyList.get(course).size() == 0) {
+            return true;
+        }
+
+        cycle.add(course);
+        for (Integer preqCourse : adjancenyList.get(course)) {
+            // If 1 is false the result would be false, else true
+            if (!isCoursePossibleToFinish(preqCourse, adjancenyList, cycle)) {
+                return false;
+            }
+        }
+
+        // Important: Remove the current one from the set
+        cycle.remove(course);
+        adjancenyList.put(course, new ArrayList<>());
+        return true;
     }
 
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
